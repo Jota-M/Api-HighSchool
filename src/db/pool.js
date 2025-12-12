ab5c275b0924cebd4b781a37a96d99e06b3ede89
JoA <cooper17john@gmail.com>
2025-12-12 00:37:45 -0400

    test deploy render finnnnal

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,10 +1,16 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 import { getEnv } from '../config/env.js';
+import dns from 'dns';
+
+// Forzar resolución DNS a IPv4
+dns.setDefaultResultOrder('ipv4first');
 
 dotenv.config();
 
 const env = getEnv();
+
+console.log('Pool config:', env.connectionString ? 'Using connectionString' : 'Using individual params');
 
 export const pool = new pg.Pool(
   env.connectionString 
